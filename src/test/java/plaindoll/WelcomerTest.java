aa3b0af68e3ed30e1633a6f76a033376637ddfd4
dvps <mastadrummie@gmail.com>
2022-09-21 00:54:10 +0300

    test hunter

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -32,7 +32,7 @@ public class WelcomerTest {
 	}
 
 	@Test
-	public void welcomerSaysSomethingHunter(){
+	public void welcomerSaysSomeHunter(){
 		assertThat(welcomer.saySome(), containsString("hunter"));
 	}
 }
